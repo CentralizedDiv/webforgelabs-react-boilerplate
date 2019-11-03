e0b14991eaf122d67468b22df59630f5029fc66f
Arthur Melo <arthurhsmelo@gmail.com>
2019-11-03 19:54:42 -0300

    dsm

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,3 +2,6 @@ import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-notes/register';
+import { registerDsm } from '@invisionapp/dsm-storybook/register';
+
+registerDsm(process.env.STORYBOOK_DSM);
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { configure } from '@storybook/react';
 import { addDecorator } from '@storybook/react/dist/client/preview';
+import { initDsm } from '@invisionapp/dsm-storybook';
 import 'config/i18n';
 
 // Automatically import all files ending in *.stories.tsx/.ts inside src
@@ -8,6 +9,13 @@ const req = require.context('../src', true, /\.stories\.ts|\.stories\.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+//Init Dsm
+initDsm({
+  addDecorator,
+  addParameters,
+  callback: () => configure(loadStories, module),
+});
 
 const withGlobalStyle = cb => <div style={{ margin: 10 }}>{cb()}</div>;
 
